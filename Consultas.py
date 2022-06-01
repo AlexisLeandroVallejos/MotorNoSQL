0f0dc0bd589dbb7f63d4968057cd13a56016e01f
AlexisLeandroVallejos <alexv4lle@gmail.com>
2022-06-01 10:09:48 -0300

    agregada consulta uno

--- Consultas.py
+++ Consultas.py
@@ -10,4 +10,24 @@ mongoimport --db test --collection tweets --drop --file=tweets.json
 *Para instalar MongoImport: https://www.mongodb.com/docs/database-tools/installation/installation-windows/#installation
 """
 
+from pymongo import MongoClient
+#constantes
+HOST = "localhost"
+NUMERO_DE_PUERTO = 27017
+NOMBRE_DATABASE = "test"
+NOMBRE_COLLECTION = "tweets"
 
+#parametros
+LOS_PRIMEROS_DIEZ = 10
+
+#agregar conexion al cliente
+client = MongoClient(HOST, NUMERO_DE_PUERTO)
+
+database = client[NOMBRE_DATABASE]
+collection = database[NOMBRE_COLLECTION]
+
+#1ra Consulta Simple:
+consultaUno = collection.find({}, {"_id": 1, "text": 1}).limit(LOS_PRIMEROS_DIEZ)
+
+for elemento in consultaUno:
+    print(elemento)
